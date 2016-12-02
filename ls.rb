a2f6ddebf7b5555af0e17df95589de156e424376
itggot-jimmy-karlheden <jimmy.karlheden@itggot.se>
2016-12-02 15:14:00 +0100

    ls.rb

--- ls.rb
+++ ls.rb
@@ -1,1 +1,11 @@
-require_relative 'lib/.rb'
+a = ARGV[0]
+
+if ARGV == []
+  puts "put in a argument next time, stupid.."
+  exit
+elsif a == "txt" || a == "rb"
+ puts Dir.glob ("*.#{a}")
+else
+  puts "i don't care, do soemthing about your life."
+  puts "there is nothing called #{a}"
+end
