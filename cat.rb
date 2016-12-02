266389874e414fe562ba2c0e8d717aa0e711003a
itggot-jimmy-karlheden <jimmy.karlheden@itggot.se>
2016-12-02 15:00:21 +0100

    Revert "Revert "touch.rb""
    This reverts commit f48465607480f7d8aa445ce2f75b64ce9fbfdca6.

--- cat.rb
+++ cat.rb
@@ -1,1 +1,5 @@
-require_relative 'lib/.rb'
+if File.exist? (ARGV[0])
+  puts File.readlines(ARGV[0])
+else
+  puts "Error! there is no file with that name!"
+end
--- touch.rb
+++ touch.rb
@@ -1,1 +1,7 @@
-require_relative 'lib/.rb'
+if
+   File.exist? (ARGV[0])
+puts "ERROR: this file already exist!"
+else
+  File.open(ARGV[0], "w") do |file|
+  end
+end
